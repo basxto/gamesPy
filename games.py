ad2db960eab8b0da8771815832d493230d2c02cc
Sebastian Riedel <git@basxto.de>
2017-11-13 23:09:36 +0100

    Use functions, also tell when game got closed

--- games.py
+++ games.py
@@ -41,46 +41,51 @@ trackedGames = {
 }
 
 found = {'pid': -1, 'name': '', 'started': 0}
-try:
-    while 1:
-        for proc in psutil.process_iter():
-            try:
-                pinfo = proc.as_dict(attrs=['pid', 'name', 'create_time', 'cwd', 'cmdline', 'environ'])
-            except psutil.NoSuchProcess:
-                pass
-            else:
-                for name, game in trackedGames.items():
-                    for trackedProcess in game['processes']:
-                        if pinfo['name'] == trackedProcess['name'] and ( ('argument' not in trackedProcess) or (trackedProcess['argument'] in pinfo['cmdline'])):
-                            found['pid'] = pinfo['pid'];
-                            found['name'] = name;
-                            found['startedu'] = pinfo['create_time'];
-                            notify2.Notification(name + ' started', 'Has PID ' + str(pinfo['pid']) + ' and was started ' + datetime.datetime.fromtimestamp(pinfo['create_time']).strftime("%Y-%m-%d %H:%M:%S")).show();
-                            print(name + ' started', 'Has PID ' + str(pinfo['pid']) + ' and was started ' + datetime.datetime.fromtimestamp(pinfo['create_time']).strftime("%Y-%m-%d %H:%M:%S"));
-                            break;
-                    if found['pid'] != -1:
-                        break
-                if found['pid'] != -1:
-                    break;
-        while found['pid'] != -1:
-            try:
-                p = psutil.Process(found['pid']);
-                pinfo = p.as_dict(attrs=['pid', 'name', 'create_time', 'cwd', 'cmdline', 'environ'])
-                
-            except psutil.NoSuchProcess:
-                notify2.Notification(found['name'] + " closed").show();
-                print(found['name'] + " closed");
-                found['pid'] = -1;
-            else:
-                for con in p.connections():
-                    if con.status == psutil.CONN_ESTABLISHED:
-                        print(con);
+
+def note(head, msg):
+    notify2.Notification(head, msg).show();
+    print(head + ":\n  " + msg);
+
+def track():
+    try:
+        while 1:
+            for proc in psutil.process_iter():
                 try:
-                    #wait for process to exit or 5 seconds
-                    p.wait(5);
-                except psutil.TimeoutExpired:
-                    pass;
-        time.sleep(10);
-except KeyboardInterrupt:
-    print('Stopped listening for newly started games...');
-    print('Good bye');
+                    pinfo = proc.as_dict(attrs=['pid', 'name', 'create_time', 'cwd', 'cmdline', 'environ'])
+                except psutil.NoSuchProcess:
+                    pass
+                else:
+                    for name, game in trackedGames.items():
+                        for trackedProcess in game['processes']:
+                            if pinfo['name'] == trackedProcess['name'] and ( ('argument' not in trackedProcess) or (trackedProcess['argument'] in pinfo['cmdline'])):
+                                found['pid'] = pinfo['pid'];
+                                found['name'] = name;
+                                found['startedu'] = pinfo['create_time'];
+                                note(name + ' started', 'Has PID {pid} and was started {start}'.format(pid=pinfo['pid'],start=datetime.datetime.fromtimestamp(pinfo['create_time']).strftime("%Y-%m-%d %H:%M:%S")));
+                                break;
+                        if found['pid'] != -1:
+                            break
+                    if found['pid'] != -1:
+                        break;
+            while found['pid'] != -1:
+                try:
+                    p = psutil.Process(found['pid']);
+                    pinfo = p.as_dict(attrs=['pid', 'name', 'create_time', 'cwd', 'cmdline', 'environ'])
+                except psutil.NoSuchProcess:
+                    note(found['name'] + " closed", 'Ended {end}'.format(end=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")));
+                    found['pid'] = -1;
+                else:
+                    for con in p.connections():
+                        if con.status == psutil.CONN_ESTABLISHED:
+                            print(con);
+                    try:
+                        #wait for process to exit or 5 seconds
+                        p.wait(5);
+                    except psutil.TimeoutExpired:
+                        pass;
+            time.sleep(10);
+    except KeyboardInterrupt:
+        print('Stopped listening for newly started games...');
+        print('Good bye');
+
+track();

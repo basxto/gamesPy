cfd953ca0179a10c8b94d07db110699429847d10
Sebastian Riedel <git@basxto.de>
2018-09-08 15:22:26 +0200

    Add games and sessions to restful api

--- src/api.py
+++ src/api.py
@@ -2,11 +2,13 @@ import logging
 import subprocess
 import datetime
 import storage
+import math
 
 class Api:
     def __init__(self, config):
         self.config = config
 
+    #thread safe
     def getGames(self):
         store = storage.Database(self.config['DATABASE']['path'], False)
         trackedGames = {}
@@ -16,16 +18,18 @@ class Api:
     def onStart(self, game, started, pid):
         logging.info('{name} has PID {pid} and was started {start}'.format(name=game.name,pid=pid,start=datetime.datetime.fromtimestamp(started).strftime("%Y-%m-%d %H:%M:%S")))
         subprocess.Popen(self.config['RUN']['onstart'].format(name=game.name, id=game.monitorid, start=started), shell=True)
+
+    def deltaToDict(self, delta):
+        seconds = (delta.seconds)%60
+        minutes = math.floor( (seconds/60)%60 )
+        hours = math.floor( minutes/60 )
+        return {'seconds': seconds, 'minutes': minutes, 'hours': hours}
         
     
     def onQuit(self, session):
         sg=session.game.saveGame
-        seconds = session.getDuration().seconds
-        minutes = seconds/60
-        hours = minutes/60
-        totSeconds = session.game.getPlaytime().seconds
-        totMinutes = totSeconds/60
-        totHours = totMinutes/60
-        logging.info('This session of {} took {}h {}min {}sec'.format(session.game.name, round(hours),round(minutes%60),seconds%60))
-        logging.info('You played {} {}h {}min {}sec in total'.format(session.game.name, round(totHours%24),round(totMinutes%60),totSeconds%60))
-        subprocess.Popen(self.config['RUN']['onquit'].format(name=session.game.name, id=session.game.monitorid, savePath=sg.path, saveInclude=sg.include, saveExclude=sg.exclude, h=round(hours%24),m=round(minutes%60),s=seconds%60, start=session.start.timestamp()), shell=True)
+        duration = self.deltaToDict(session.getDuration())
+        played = self.deltaToDict(session.game.getPlaytime())
+        logging.info('This session of {} took {}h {}min {}sec'.format(session.game.name, duration.hours,duration.minutes,duration.seconds))
+        logging.info('You played {} {}h {}min {}sec in total'.format(session.game.name, played.hours,played.minutes,played.seconds))
+        subprocess.Popen(self.config['RUN']['onquit'].format(name=session.game.name, id=session.game.monitorid, savePath=sg.path, saveInclude=sg.include, saveExclude=sg.exclude, h=duration.hours,m=duration.minutes,s=duration.seconds, start=session.start.timestamp()), shell=True)
--- src/server.py
+++ src/server.py
@@ -1,23 +1,46 @@
 #!/usr/bin/python3
 from flask import Flask
 from flask import jsonify
+from flask import abort
 import logging
 import threading
+import games
 #import api
 app = Flask(__name__)
 appApi = None
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
-@app.route('/getGames')
-def start():
-    ##Api.onStart();
+    return 'This is the RESTful API of games.py'
+
+@app.route('/game/list')
+def allGames():
     trackedGames = appApi.getGames()
     games = {}
     for monitorid, game in trackedGames.items():
         games[monitorid] = game.name
     return jsonify(games)
+
+@app.route('/game/<id>')
+def game(id):
+    trackedGames = appApi.getGames()
+    if id in trackedGames:
+        game = trackedGames[id]
+        return jsonify(id = id, name = game.name, process = game.process, argument = game.argument, processPath = game.processPath, playTime = appApi.deltaToDict(game.getPlaytime()))
+    else:
+        abort(404)
+
+@app.route('/game/<id>/sessions')
+def gameSessions(id):
+    trackedGames = appApi.getGames()
+    if id in trackedGames:
+        game = trackedGames[id]
+        sessions = []
+        for session in game.sessions:
+            sessions.append({"start":session.start.timestamp(),"end":session.end.timestamp()})
+        return jsonify(sessions)
+    else:
+        abort(404)
 def flaskThread(host,port,api):
     global appApi
     appApi = api
